4d977e3d25896383de7b04325ae8165803074398
JiyongO <52910721+JiyongO@users.noreply.github.com>
2019-09-19 16:05:17 +0900

    Chapter01_Done

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class GameManager : MonoBehaviour
+    {
+        public Texture2D levelTexture;
+        Texture2D textureInstance;
+        public SpriteRenderer levelRenderer;
+
+        public float posOffset = 0.01f;
+        int maxX;
+        int maxY;
+        Node[,] grid;
+
+        Vector3 mousePos;
+        Node curNode;
+        Node prevNode;
+
+        public Transform spawnTransform;
+        [HideInInspector]
+        public Node spawnNode;
+        [HideInInspector]
+        public Vector3 spawnPosition;
+
+        public Unit unit;
+
+        public static GameManager singleton;
+        private void Awake()
+        {
+            singleton = this;
+        }
+        private void Start()
+        {
+            CreateLevel();
+            spawnNode = GetNodeFromWorldPos(spawnTransform.position);
+            spawnPosition = GetWorldPosFromNode(spawnNode);
+            unit.Init(this);
+        }
+
+        void CreateLevel()
+        {
+            maxX = levelTexture.width;
+            maxY = levelTexture.height;
+            grid = new Node[maxX, maxY];
+            textureInstance = new Texture2D(maxX, maxY);
+            textureInstance.filterMode = FilterMode.Point;
+
+            for (int x = 0; x < maxX; x++)
+            {
+                for (int y = 0; y < maxY; y++)
+                {
+                    Node n = new Node();
+                    n.x = x;
+                    n.y = y;
+
+                    Color c = levelTexture.GetPixel(x, y);
+                    textureInstance.SetPixel(x, y, c);
+                    n.isEmpty = (c.a == 0);
+
+                    grid[x, y] = n;
+                }
+            }
+
+            textureInstance.Apply();
+            Rect rect = new Rect(0, 0, maxX, maxY);
+            levelRenderer.sprite = Sprite.Create(textureInstance, rect, Vector2.zero);
+        }
+
+        private void Update()
+        {
+            GetMousePosition();
+            HandleMouseInput();
+        }
+
+        void HandleMouseInput()
+        {
+            if (curNode == null)
+                return;
+
+            if (Input.GetMouseButton(0))
+            {
+                if (curNode != prevNode)
+                {
+                    prevNode = curNode;
+
+                    Color c = Color.white;
+                    c.a = 0;
+
+                    for (int x = -6; x < 6; x++)
+                    {
+                        for (int y = -6; y < 6; y++)
+                        {
+                            int t_x = x + curNode.x;
+                            int t_y = y + curNode.y;
+
+                            Node n = GetNode(t_x, t_y);
+                            if (n == null)
+                                continue;
+
+                            n.isEmpty = true;
+                            textureInstance.SetPixel(t_x, t_y, c);
+                        }
+                    }
+
+                    textureInstance.Apply();
+                }
+            }
+        }
+
+        void GetMousePosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            mousePos = ray.GetPoint(5);
+            curNode = GetNodeFromWorldPos(mousePos);
+        }
+
+        public Node GetNodeFromWorldPos(Vector3 wp)
+        {
+            int t_x = Mathf.RoundToInt(wp.x / posOffset);
+            int t_y = Mathf.RoundToInt(wp.y / posOffset);
+
+            return GetNode(t_x, t_y);
+        }
+
+        public Node GetNode(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > maxX - 1 || y > maxY - 1)
+                return null;
+            return grid[x, y];
+        }
+
+        public Vector3 GetWorldPosFromNode(int x, int y)
+        {
+            Vector3 r = Vector3.zero;
+            r.x = x * posOffset;
+            r.y = y * posOffset;
+            return r;
+        }
+
+        public Vector3 GetWorldPosFromNode(Node n)
+        {
+            if (n == null)
+                return -Vector3.one;
+
+            Vector3 r = Vector3.zero;
+            r.x = n.x * posOffset;
+            r.y = n.y * posOffset;
+            return r;
+        }
+    }
+
+    public class Node
+    {
+        public int x;
+        public int y;
+        public bool isEmpty;
+    }
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class Unit : MonoBehaviour
+    {
+        bool isInit;
+        Node curNode;
+        Node targetNode;
+
+        public bool move;
+        public float lerpSpeed = 1;
+        float baseSpeed;
+        bool initLerp;
+        Vector3 targetPos;
+        Vector3 startPos;
+        float t;
+        GameManager gameManager;
+        int t_x;
+        int t_y;
+
+        public void Init(GameManager gm)
+        {
+            gameManager = gm;
+            PlaceOnNode();
+            isInit = true;
+        }
+
+        void Start()
+        {
+
+        }
+
+        void PlaceOnNode()
+        {
+            curNode = gameManager.spawnNode;
+            transform.position = gameManager.spawnPosition;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isInit)
+                return;
+            if (!move)
+                return;
+            if (!initLerp)
+            {
+                initLerp = true;
+                startPos = transform.position;
+                t = 0;
+                Pathfind();
+                Vector3 tp = gameManager.GetWorldPosFromNode(targetNode);
+                targetPos = tp;
+                float d = Vector3.Distance(targetPos, startPos);
+                baseSpeed = lerpSpeed / d;
+            }
+            else
+            {
+                t += Time.deltaTime * baseSpeed;
+                if (t > 1)
+                {
+                    t = 1;
+                    initLerp = false;
+                    curNode = targetNode;
+                }
+
+                Vector3 tp = Vector3.Lerp(startPos, targetPos, t);
+                transform.position = tp;
+            }
+        }
+
+        void Pathfind()
+        {
+            t_x = curNode.x;
+            t_y = curNode.y;
+
+            Node nextDown = gameManager.GetNode(t_x, t_y - 1);
+            if (nextDown == null)
+                return;
+            if (!nextDown.isEmpty)
+            {
+                t_y = curNode.y;
+            }
+            else
+            {
+                t_y -= 1;
+            }
+
+            targetNode = gameManager.GetNode(t_x, t_y);
+        }
+    }
+}
